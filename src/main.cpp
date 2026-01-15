3b5393d3ba809434558af0860f1f381a9cfadc71
Kristjan <kristjan.pet@gmail.com>
2026-01-15 12:16:07 +0100

    Added struct sending

--- src/main.cpp
+++ src/main.cpp
@@ -2,38 +2,47 @@
 #include "freertos/task.h"
 #include "freertos/queue.h"
 #include "esp_log.h"
+#include "esp_timer.h"
+
+struct Sample {
+    int count;
+    uint32_t timestamp_ms;
+};
 
 QueueHandle_t sampleQueue;
 
 static const char *TAG = "MAIN";
 
 void producer_task(void *pvParameters){
+    Sample s;
     int count = 0;
     while(1){
-        if(xQueueSend(sampleQueue, &count, pdMS_TO_TICKS(50)) != pdTRUE){ //sending data
-            ESP_LOGI("sender", "queue full at: %d", count);
+        s.count = count;
+        s.timestamp_ms = (uint32_t)(esp_timer_get_time() / 1000);
+        if(xQueueSend(sampleQueue, &s, pdMS_TO_TICKS(50)) != pdTRUE){ //sending data
+            ESP_LOGW("sender", "queue full at: %d", s.count);
         }
         else{
-            ESP_LOGI("sender", "sent: %d", count);
-            count++;
+            ESP_LOGI("sender", "sent: %d", s.count);
         }
-        vTaskDelay(pdMS_TO_TICKS(2000)); //delays task, ms_to_ticks converts to ms
+        count++;
+        vTaskDelay(pdMS_TO_TICKS(200)); //delays task, ms_to_ticks converts to ms
     }
     
 }
 
 void consumer_task(void *pvParameters){
-    int value;
+    Sample s;
     while(1){
-        if(xQueueReceive(sampleQueue, &value, portMAX_DELAY) != pdTRUE){
+        if(xQueueReceive(sampleQueue, &s, portMAX_DELAY) != pdTRUE){
             ESP_LOGE("consumer", "receiving data failed");
         };
-        ESP_LOGI("consumer", "got: %d", value);
+        ESP_LOGI("consumer", "got: %d with time: %u", s.count, s.timestamp_ms);
     }
 }
 
 extern "C" void app_main(void) {
-    sampleQueue = xQueueCreate(5, sizeof(int)); //creates queue
+    sampleQueue = xQueueCreate(5, sizeof(Sample)); //creates queue
     if (sampleQueue == NULL){
         ESP_LOGE(TAG, "Failed to create sampleQueue");
         return;
