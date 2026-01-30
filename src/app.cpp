5b827e43ba7ccdb9b554621d0561d53461381fe5
Kristjan <kristjan.pet@gmail.com>
2026-01-30 19:24:50 +0100

    Add LED control during button press in ISR handling

--- src/app.cpp
+++ src/app.cpp
@@ -36,6 +36,18 @@ bool App::start(){
         return false;
     }
 
+    //LED setup
+    gpio_config_t led_conf{};
+    led_conf.intr_type = GPIO_INTR_DISABLE;
+    led_conf.mode = GPIO_MODE_OUTPUT;
+    led_conf.pin_bit_mask = 1ULL << GPIO_NUM_2;
+    led_conf.pull_up_en = GPIO_PULLUP_DISABLE;
+    led_conf.pull_down_en = GPIO_PULLDOWN_DISABLE;
+    ESP_ERROR_CHECK(gpio_config(&led_conf));
+
+    gpio_set_level(GPIO_NUM_2, 0); // start OFF
+
+
     //intall and register ISR
     ESP_ERROR_CHECK(gpio_install_isr_service(0));
     ESP_ERROR_CHECK(gpio_isr_handler_add(GPIO_NUM_4, gpio_isr_handler, this));
@@ -325,6 +337,7 @@ void App::button_trampoline(void *pv){
 }
 
 void App::button(){
+    bool ledOn = false;
     const TickType_t debounce = pdMS_TO_TICKS(50);
 
     while(true){
@@ -333,9 +346,16 @@ void App::button(){
         if(ctx_.stopRequested) break;
 
         vTaskDelay(debounce);
-        int level = gpio_get_level(GPIO_NUM_4);
-        if(level == 0){
-            ESP_LOGI("BTN", "Button pressed");
+
+        while(gpio_get_level(GPIO_NUM_4) == 0){
+            ledOn = !ledOn;
+            gpio_set_level(GPIO_NUM_2, ledOn);
+            ESP_LOGI("BTN", "Pressed LED=%d", (int)ledOn);
+
+            // while (gpio_get_level(GPIO_NUM_4) == 0){ //wait for release
+            //     vTaskDelay(pdMS_TO_TICKS(10));
+            // }
+            vTaskDelay(pdMS_TO_TICKS(500));
         }
     }
 
